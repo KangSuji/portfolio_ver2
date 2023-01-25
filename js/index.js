8255ebe9ba7f5dd774e0ea31bd417ad8f0c2f7e0
SUJI <sukang0608@naver.com>
2023-01-25 18:50:11 +0900

    add sub page

--- js/index.js
+++ js/index.js
@@ -19,38 +19,3 @@ $(document).ready(function () {
     horizontal: true,
   });
 });
-
-$(window).scroll(function () {
-  if ($(window).scrollTop() >= $(".right-box").offset().top) {
-    $(".go-git__btn").addClass("sticky");
-  }
-
-  if ($(window).scrollTop() <= $(".right-box").offset().top) {
-    $(".go-git__btn").removeClass("sticky");
-  }
-
-  if ($(window).scrollTop() >= $("#p-ncs").offset().top) {
-    $(".go-git__btn").attr("href", "https://github.com/KangSuji/NCS");
-  }
-  if ($(window).scrollTop() >= $("#p-kiada").offset().top) {
-    $(".go-git__btn").attr("href", "https://github.com/KangSuji/kiada");
-  }
-  if ($(window).scrollTop() >= $("#p-teasung").offset().top) {
-    $(".go-git__btn").attr("href", "https://github.com/KangSuji/taesung");
-  }
-  if ($(window).scrollTop() >= $("#p-amom").offset().top) {
-    $(".go-git__btn").attr("href", "");
-  }
-  if ($(window).scrollTop() >= $("#p-atx").offset().top) {
-    $(".go-git__btn").attr("href", "https://github.com/KangSuji/ATX_VER02");
-  }
-  if ($(window).scrollTop() >= $("#p-news").offset().top) {
-    $(".go-git__btn").attr("href", "https://github.com/KangSuji/vue3-news");
-  }
-  if ($(window).scrollTop() >= $("#p-omdb").offset().top) {
-    $(".go-git__btn").attr("href", "https://github.com/KangSuji/vue-movie-app");
-  }
-  if ($(window).scrollTop() >= $("#p-starbucks").offset().top) {
-    $(".go-git__btn").attr("href", "https://github.com/KangSuji/starbucks");
-  }
-});
